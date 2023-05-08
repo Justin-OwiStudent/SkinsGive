95b178926ee3a97a3bdbbeef49f209bb97a83880
justin koster <200100@virtualwindow.co.za>
2023-05-09 01:10:42 +0200

    almost finished main competition page

---
+++ skinsgive/Screens/MainScreen.jsx
@@ -0,0 +1,87 @@
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CompetitionCards from '../components/CompetitionCards'
+
+const MainScreen = () => {
+
+    dummyData = [
+        {title: 'Project 1', year: 2023, creator: 'Armand', tech: ["React Native", 'Firebase']},
+        {title: 'Project 2', year: 2023, creator: 'Armand', tech: ["React Native", 'Firebase']},
+    ]
+
+  return (
+    <View  style={styles.container}>
+        
+      <View style={styles.bar} >
+      <Image style={styles.logo} source={require("../assets/CompLogo.png")}/>
+
+      </View>  
+
+      <Text style={styles.title}> Ongoing Competitions </Text>
+
+      <ScrollView>
+        {dummyData.map((project, index) => (
+            <CompetitionCards key={index} data={project}/>
+        ))}
+    </ScrollView>
+
+    <View style={styles.navBar}> 
+      <Image style={styles.navIcon} source={require("../assets/CompLogo.png")}/>
+      <Image style={styles.navIcon} source={require("../assets/CompLogo.png")}/>
+      <Image style={styles.navIcon} source={require("../assets/CompLogo.png")}/>
+
+    </View>
+
+    </View> //container 
+    
+  )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({
+ container: {
+    padding: 20   
+ },
+ bar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: '#393B3F',
+    alignSelf: 'center',
+    marginBottom: 15,
+    marginTop: 25,
+    resizeMode: 'contain',
+    borderWidth: 1,
+    borderColor: '#A12895',
+ },
+ logo: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover'
+
+ },
+ title: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#A12895',
+    marginBottom: 15,
+
+ },
+ navBar: {
+    width: 300,
+    height: 80,
+    backgroundColor: '#393B3F',
+    marginBottom: 25,
+    alignSelf: 'center',
+    borderRadius: 25,
+    zIndex: 999,
+    flexDirection: 'row',
+    paddingLeft: 5
+ },
+ navIcon: {
+    width: 75,
+    height: 75,
+    marginRight: 35
+ }
+})
---
+++ skinsgive/components/CompetitionCards.jsx
@@ -0,0 +1,101 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CompetitionCards = () => {
+  return (
+    <View style={styles.card}>
+        <View style={styles.timer}>
+            <Text style={styles.time}> 00:00:00 </Text>
+        </View>
+        <View style={styles.skinSection}>
+            <Image style={styles.skin} source={require("../assets/Howl.png")}/>
+            <Text style={styles.skinName}> M4A4 Howl</Text>
+        </View>
+        <View style={styles.enties}>
+            <Text style={styles.entriesText}> Entries: 0</Text>
+            <View style={styles.btn}>
+                <Text style={styles.enterText}> Enter Competition</Text>
+            </View>
+
+        </View>
+        {/* <View style={styles.timer}>  </View> */}
+        {/* <View style={styles.skinSection}></View> */}
+      {/* <Text>CompetitionCards</Text> */}
+    </View>
+  )
+}
+
+export default CompetitionCards
+
+const styles = StyleSheet.create({
+    card: {
+        width: 350,
+        height: 250,
+        backgroundColor: '#2E3034',
+        marginTop: 25,
+        borderRadius: 25
+    },
+    timer: {
+        width: 100,
+        height: 30,
+        backgroundColor: '#A12895',
+        alignSelf: 'center',
+        marginTop: 10,
+        borderRadius: 20,
+        padding: 6
+    },
+    time: {
+        textAlign: 'center',
+        color: 'white'
+        
+    },
+    skinSection: {
+        width: 300,
+        height: 125,
+        backgroundColor: '#393B3F',
+        alignSelf: 'center',
+        marginTop: 10,
+        borderRadius: 20
+    },
+    skin: {
+        height: 65,
+        width: 300,
+        alignSelf: 'center',
+        marginBottom: 20,
+        marginTop: 20,
+        resizeMode: 'cover'
+    },
+    skinName: {
+        textAlign: 'center',
+        color: 'white',
+        // textDecorationLine: 'underline'
+    },
+    enties: {
+        width: 300,
+        height: 50,
+        // backgroundColor: 'red',
+        alignSelf: 'center',
+        marginTop: 10
+    },
+    entriesText: {
+        fontSize: 12,
+        marginTop: 15,
+        color: 'white',
+        alignSelf: 'flex-start'
+    },
+    btn: {
+        width:150,
+        height: 40,
+        marginTop: 5,
+        marginRight: 5,
+        borderRadius: 25,
+        backgroundColor: '#A12895',
+        position: 'absolute',
+        right: 0
+    },
+    enterText: {
+        color: 'white',
+        textAlign: 'center',
+        marginTop: 10
+    }
+})
